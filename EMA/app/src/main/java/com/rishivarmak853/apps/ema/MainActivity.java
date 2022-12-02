8eb60fbc772d87447d2b5764492b41d9bfb64d26
Rishi Varma Kalidindi <rishivarma.lc.dev@gmail.com>
2022-12-02 13:20:15 -0500

    Project File Created

---
+++ EMA/app/src/main/java/com/rishivarmak853/apps/ema/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rishivarmak853.apps.ema;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
