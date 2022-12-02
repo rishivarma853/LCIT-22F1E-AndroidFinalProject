f8b6c93e28aa4063e472f057e59eb520d444517b
Rishi Varma Kalidindi <rishivarma.lc.dev@gmail.com>
2022-12-02 15:49:44 -0500

    Added the Home Page Activity to the App

---
+++ EMA/app/src/main/java/com/rishivarmak853/apps/ema/HomePageActivity.java
@@ -0,0 +1,14 @@
+package com.rishivarmak853.apps.ema;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
--- EMA/app/src/main/java/com/rishivarmak853/apps/ema/LoadingPageActivity.java
+++ EMA/app/src/main/java/com/rishivarmak853/apps/ema/LoadingPageActivity.java
@@ -2,7 +2,9 @@ package com.rishivarmak853.apps.ema;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class LoadingPageActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class LoadingPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loadingpage);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Handler handler = new Handler();
+        handler.postDelayed(()-> {
+            startActivity(new Intent(LoadingPageActivity.this,));
+        },3000)
     }
 }
