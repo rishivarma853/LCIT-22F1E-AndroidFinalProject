950cbc54593a0491636d72099affa0fde63929ae
Rishi Varma Kalidindi <rishivarma.lc.dev@gmail.com>
2022-12-02 16:02:41 -0500

    Added the Activity Transitions

--- EMA/app/src/main/java/com/rishivarmak853/apps/ema/HomePageActivity.java
+++ EMA/app/src/main/java/com/rishivarmak853/apps/ema/HomePageActivity.java
@@ -3,6 +3,7 @@ package com.rishivarmak853.apps.ema;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.transition.Explode;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        getWindow().setEnterTransition(new Explode());
+        getWindow().setExitTransition(new Explode());
     }
 }
--- EMA/app/src/main/java/com/rishivarmak853/apps/ema/LoadingPageActivity.java
+++ EMA/app/src/main/java/com/rishivarmak853/apps/ema/LoadingPageActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.transition.Explode;
 
 public class LoadingPageActivity extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class LoadingPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loadingpage);
+        getWindow().setEnterTransition(new Explode());
+        getWindow().setExitTransition(new Explode());
     }
 
     @Override
@@ -20,6 +23,7 @@ public class LoadingPageActivity extends AppCompatActivity {
         Handler handler = new Handler();
         handler.postDelayed(()-> {
             startActivity(new Intent(LoadingPageActivity.this,HomePageActivity.class));
+            finish();
         },3000);
     }
 }
